561def064acba07305569b9f6d70f5a52a2d356c
huguanghui <guanghui.hu@simshine.ai>
2022-10-24 18:37:35 +0800

    feat 支持指定设备参数获取

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-import router from './router'
-import { BootstrapVue3 } from 'bootstrap-vue-3'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue3 } from "bootstrap-vue-3";
+import axios from "axios";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-const app = createApp(App)
-app.use(BootstrapVue3)
-app.use(router)
-app.mount('#app')
+axios.defaults.baseURL = "/";
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+axios.defaults.timeout = 5000;
+
+const app = createApp(App);
+
+app.config.globalProperties.$http = axios;
+app.use(BootstrapVue3);
+app.use(router);
+app.use(store);
+app.mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore } from "vuex";
+import state from "./state";
+
+export default createStore({
+  state,
+  modules: {},
+});
